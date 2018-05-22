77d4a0a58ed05a62f8fd284828294d0205357dfc
dannymcwaves <dannymcwaves96@gmail.com>
2018-05-22 12:03:26 +0000

    when timer is still tracking, show a notification prompting the user to quit?

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -10,12 +10,14 @@ import "@angular/common";
 import "@angular/http";
 import "@angular/router";
 import "rxjs";
+import * as moment from "moment";
 
 // main
 
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
 import { AppModule } from "./app/app.module";
+import { remote, ipcRenderer } from 'electron';
 
 if(process.env.NODE_ENV === "production") {
     enableProdMode();
@@ -23,8 +25,32 @@ if(process.env.NODE_ENV === "production") {
 
 // add base element for Angular Router support
 const base = document.createElement('base');
+window['timeIsRunning'] = false;
+
 // this is './' not '/' because electron uses file:// URLs
 base.href = './';
 document.head.appendChild(base);
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+window.onbeforeunload = (event: any) => {
+  if (window['timeIsRunning']) {
+
+    event.returnValue = false;
+
+    if (confirm("You're currently tracking time, are you sure you want to quit?")) {
+
+      ipcRenderer.send("timer", {
+        action: "stop",
+        date: moment().milliseconds(0).toISOString()
+      });
+
+      window['timeIsRunning'] = false;
+      event.returnValue = true;
+
+      // let win = remote.getCurrentWindow();
+      window.close();
+    }
+  }
+};

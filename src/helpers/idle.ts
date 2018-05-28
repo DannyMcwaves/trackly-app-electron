1e332f47347bd00e8337c223e783b2fffab9ead2
dannymcwaves <dannymcwaves96@gmail.com>
2018-05-28 14:08:01 +0000

    idler program

--- src/helpers/idle.ts
+++ src/helpers/idle.ts
@@ -1,53 +1,53 @@
-//
-// import * as threads from 'threads';
-// import * as desktopIdle from 'desktop-idle';
-// import {Fscs} from './fscs';
-//
-// export class Idler {
-//
-//   private _totalIdleTime:number = 0;
-//
-//   constructor(private fscs: Fscs) {}
-//
-//   private idleTime() {
-//     return desktopIdle.getIdleTime();
-//   }
-//
-//   public get idle() {
-//     this._totalIdleTime += this.idleTime();
-//     return this._totalIdleTime;
-//   }
-//
-//   public logTick(tick:any) {
-//     // console.log(tick);
-//     console.log(this.idleTime());
-//   }
-//
-// }
-//
-// //
-// // const spawn = threads.spawn;
-// //
-// // const thread = spawn(function (input:any, done: any) {
-// //   console.log(input);
-// //   return done({idleTime: input()})
-// // });
-// //
-// // export default function() {
-// //   return new Promise((resolve, reject) => {
-// //     thread
-// //       .send({string: '123'})
-// //       .on('message', (res:any) => {
-// //         resolve(res)
-// //       })
-// //       .on('error', (err:any) => {
-// //         reject(err)
-// //       })
-// //       .on('exit', () => {
-// //         // terminated.
-// //       })
-// //   });
-// //
-// // }
-// //
-// // console.log(desktopIdle);
+
+import * as desktopIdle from 'desktop-idle';
+import {Fscs} from './fscs';
+import {dialog, BrowserWindow} from "electron";
+
+
+export class Idler {
+
+  private _totalIdleTime:number = 0;
+  private _window: BrowserWindow;
+
+  constructor(private fscs: Fscs) {}
+
+  createWindow() {
+    this._window = new BrowserWindow({frame: false, height: 200, width: 500});
+    this._window.loadURL(`http://localhost:5000`);
+  }
+
+  private idleTime() {
+    return desktopIdle.getIdleTime();
+  }
+
+  idleDialog(time: any) {
+    this.createWindow();
+
+    // const dialogOpts = {
+    //   type: 'info',
+    //   buttons: ['Stop', 'Continue', 'Reassign Idle Time'],
+    //   title: 'Idle Prompt',
+    //   message: 'Idle Prompt',
+    //   detail: `You have being Idle for ${time} minutes`,
+    //   checkboxLabel: 'Keep Idle Time'
+    // };
+    //
+    // dialog.showMessageBox(this._window, {}, (response, cbox) => {
+    //   console.log(response);
+    //   console.log(cbox);
+    // });
+  }
+
+  public get idle() {
+    this._totalIdleTime += this.idleTime();
+    return this._totalIdleTime;
+  }
+
+  public logTick(tick:any) {
+    const idle = this.idleTime();
+    if (idle > 60) {
+      this.idleDialog(Math.round(idle / 60));
+    }
+  }
+
+}

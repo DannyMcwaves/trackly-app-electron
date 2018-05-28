717544568a85a60ec6c42ead2e17b000fc24b171
dannymcwaves <dannymcwaves96@gmail.com>
2018-05-28 17:32:12 +0000

    dialog component

---
+++ src/renderer/app/dialog/dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, ViewEncapsulation, OnInit } from "@angular/core";
+
+@Component({
+  selector: "#app",
+  templateUrl: "/dialog.component.html",
+  encapsulation: ViewEncapsulation.None
+})
+
+
+export class DialogComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+}

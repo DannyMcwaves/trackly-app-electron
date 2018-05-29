3b7f5517a1a1dc0ecb42cb62e9ca810f730fdbcb
dannymcwaves <dannymcwaves96@gmail.com>
2018-05-29 15:08:04 +0000

    custom dialog component almost done. [WIP]

--- src/renderer/app/dialog/dialog.component.ts
+++ src/renderer/app/dialog/dialog.component.ts
@@ -1,4 +1,8 @@
+import {ipcRenderer} from "electron";
+
 import { Component, ViewEncapsulation, OnInit } from "@angular/core";
+import "./dialog.component.scss";
+
 
 @Component({
   selector: "#app",
@@ -9,8 +13,39 @@ import { Component, ViewEncapsulation, OnInit } from "@angular/core";
 
 export class DialogComponent implements OnInit {
 
-  constructor() {
+  minutes: number = 0;
+  projectName: string = 'Current Active Project';
+  allProjects: any;
 
+  constructor() {
+    // the number of minutes user was idle.
+    ipcRenderer.on('idletime', (event: any, time: number) => {
+      this.minutes = time;
+      document.getElementById('minutes').innerText = time.toString();
+    });
+
+    // set the name of the current project
+    ipcRenderer.on('currentProject', (event: any, title: string) => {
+      this.projectName = title;
+      document.getElementById('name').innerText = title;
+    });
+
+    // projects
+    ipcRenderer.on('projects', (event: any, projects: any) => {
+      this.allProjects = projects;
+    });
+  }
+
+  stopClick(check: any) {
+    ipcRenderer.send("idleResponse", {checked: check, action: 'stop'});
+  }
+
+  continueClick(check: any) {
+    ipcRenderer.send("idleResponse", {checked: check, action: 'continue'});
+  }
+
+  reassignClick() {
+    console.log('reassigned');
   }
 
   ngOnInit(): void {

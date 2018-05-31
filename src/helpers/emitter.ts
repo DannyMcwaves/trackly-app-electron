8a344c76e5f01644a04ae4bec098dc0e71743aa0
dannymcwaves <dannymcwaves96@gmail.com>
2018-05-31 18:22:43 +0000

    global static variables to store changes throught the app

---
+++ src/helpers/emitter.ts
@@ -0,0 +1,10 @@
+/*
+* this is a simple module for saving global states and accessing changed variables
+* throughout the script.
+* */
+
+export class Emitter {
+
+  public static uploadStatus: boolean = true;
+
+}

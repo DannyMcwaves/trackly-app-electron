79deacf15a25f6bb4c86a89cd4a7f18bac69fe0f
dannymcwaves <dannymcwaves96@gmail.com>
2018-06-05 12:16:15 +0000

    get the current active windows and the duration

--- src/helpers/windows.ts
+++ src/helpers/windows.ts
@@ -4,14 +4,43 @@
 
 import * as activeWindow from 'active-win';
 import * as logger from "electron-log";
+import {Fscs} from './fscs';
 
 
 export class ActiveWindow {
-  constructor() {
-    logger.log(activeWindow);
+
+  private fscs: Fscs;
+  private windows: any;
+
+  constructor(fscs: any) {
+    this.fscs = fscs;
+    this.windows = [];
   }
 
   currentWindow() {
     return activeWindow();
   }
+
+  public current(duration: any) {
+    let isavailable = false;
+    this.currentWindow().then((data: any) => {
+      let name = data.owner.name;
+      this.windows = this.windows.map((x: any) => {
+        if (x.name === name) {
+          isavailable = true;
+          return {name, duration: x.duration + duration}
+        }
+        return x
+      });
+      if (!isavailable) {
+        this.windows.push({name, duration});
+      }
+      setTimeout( () => {
+        this.fscs.appendActiveWindow(this.windows);
+      }, 800);
+    }).catch((err: any) => {
+      logger.log(err);
+    });
+  }
+
 }

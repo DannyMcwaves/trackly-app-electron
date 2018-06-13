12f7255f3eb7dc8fef59372c7dc77108e1cf2a1b
dannymcwaves <dannymcwaves96@gmail.com>
2018-06-13 10:54:29 +0000

    browser  extension server for url

---
+++ src/helpers/server.ts
@@ -0,0 +1,26 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+
+// avoid cases of CORS
+app.use(function(req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('*', (req: any, res: any) => {
+  res.send({message: "Trackly"});
+});
+
+app.post('*', (req: any, res: any) => {
+  console.log(req.body);
+  res.send({message: "Trackly", status: "message received"});
+});
+
+export default app;

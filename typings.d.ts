f1b9f3dde268e324dafc94937888f1a2b1d67865
dannymcwaves <dannymcwaves96@gmail.com>
2018-06-20 03:44:04 +0000

    active-window refactored

--- typings.d.ts
+++ typings.d.ts
@@ -1,6 +1,6 @@
 declare module 'threads';
 declare module 'desktop-idle';
-declare module 'active-win';
+declare module 'active-window';
 declare module 'express';
 declare module 'body-parser';
 declare const __static: string;
